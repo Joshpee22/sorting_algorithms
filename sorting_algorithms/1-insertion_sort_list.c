00d522654993d4f3e9f501d96cfb27f564631913
joshpee22 <owobujoshua2017@gmail.com>
2023-07-25 17:58:14 -0700

    1-insertion_sort_list.c

--- sorting_algorithms/1-insertion_sort_list.c
+++ sorting_algorithms/1-insertion_sort_list.c
@@ -1,51 +1,61 @@
 #include "sort.h"
-
+#include <stdio.h>
+void swap(listint_t **head, listint_t *mv_back, listint_t *mv_forw);
 /**
- * insertion_sort_list - sorts a linked list ussing insertion sorting algorithm
- * @list: pointer to the head of the list
- * Return: void
+ * insertion_sort_list - sort a list using insertion sort
+ * @list: list to sort
+ * Description: Function to sort a doubly linked list using
+ * the insertion sort algorithm
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head, *current, *tmp;
+	listint_t *temp, *next, *current = (*list)->next;
 
-	if (list == NULL || (*list) == NULL)
+	if (list == NULL || *list == NULL)
 		return;
-	head = *list;
-	head = head->next;
-	while (head != NULL)
+
+	while (current != NULL)
 	{
-		current = head;
-		head = head->next;
-		tmp = current->prev;
-		while (current && tmp)
+		next = current->next;
+		if (current->n < current->prev->n)
 		{
-			if (current->n < tmp->n)
+			temp = current;
+			while (temp->prev != NULL && temp->n < temp->prev->n)
 			{
-				if (current->prev != NULL)
-				{
-					current->prev->next = current->next;
-				}
-				if (current->next != NULL)
-				{
-					current->next->prev = current->prev;
-				}
-				current->next = tmp;
-				if (tmp->prev)
-					tmp->prev->next = current;
-				current->prev = tmp->prev;
-				tmp->prev = current;
-				if (current->prev == NULL)
-				{
-					*list = current;
-					print_list(*list);
-					break;
-				}
-				tmp = current->prev;
+				swap(list, temp, temp->prev);
 				print_list(*list);
-			} else
-				tmp = tmp->prev;
+			}
 		}
+
+		current = next;
 	}
+}
+
+/**
+ * swap - swap two nodes in a dll
+ * @head: head of dll to swap in
+ * @mv_back: node to move backwards
+ * @mv_forw: node to move forward
+ * Description: Function to swap the pointer positions
+ * of two nodes in a doubly linked list
+ */
+
+void swap(listint_t **head, listint_t *mv_back, listint_t *mv_forw)
+{
+	listint_t *back_prev, *forw_next;
+
+	back_prev = mv_forw->prev;
+	forw_next = mv_back->next;
+
+	if (back_prev != NULL)
+		back_prev->next = mv_back;
+	else
+		*head = mv_back;
+	mv_forw->prev = mv_back;
+	mv_forw->next = forw_next;
+	mv_back->prev = back_prev;
+	mv_back->next = mv_forw;
+	if (forw_next)
+		forw_next->prev = mv_forw;
 }
